a17b78d07bc2b68da7ecad2c7158b3463504b0c9
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-23 21:38:21 +0530

    Episode - 2 -Backend - Todoapp -index.js - load env for port, Middleware - to parse request json body

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const app = express();
 app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`);
 })
+
+//load config from env file
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+//middleware - to parse request json body
+app.use(express.json());
